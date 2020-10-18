c1e6f9a070117ba23588776586b15e3a69b058ee
Tamara Acosta <tamara.ailen.acosta@gmail.com>
2020-10-18 12:43:16 -0300

    incluyo Menu en App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 //import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Hits from './components/Hits.js';
+import Menu from './components/Menu.js';
 
 function App() {
     return ( 
         <div className="App">
             <header className = "App-header">
-                <Hits/>
+                <Menu/>
             </header>
         </div>
     );
 }
-
 export default App;
