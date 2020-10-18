c8ae6d745213f8ad81e18e41de205eeaec194062
Tamara Acosta <tamara.ailen.acosta@gmail.com>
2020-10-18 12:29:07 -0300

    Menu2

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 //import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Menu from './components/Menu.js';
+import Hits from './components/Hits.js';
 
 function App() {
     return ( 
         <div className="App">
             <header className = "App-header">
-                <Menu/>
+                <Hits/>
             </header>
         </div>
     );
