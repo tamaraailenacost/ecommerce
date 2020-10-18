bc961195ae3adc5c8defdf9642bb9d081290e97c
Tamara Acosta <tamara.ailen.acosta@gmail.com>
2020-10-18 12:25:02 -0300

    menu

---
+++ src/components/Menu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+
+class Menu extends Component {
+    render() {
+        return ( 
+            <>
+            </>
+        );
+    }
+}
+
+export default Menu;
