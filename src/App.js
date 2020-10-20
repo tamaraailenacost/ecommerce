31f8e8cafa2cd1762aa39c874147ce0b00434681
Tamara Acosta <tamara.ailen.acosta@gmail.com>
2020-10-20 20:15:30 -0300

    unistall React-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-//import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Menu from './components/Menu.js';
+
 
 function App() {
     return ( 
