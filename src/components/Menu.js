cead7f962d414fa3c2c9b7465b9abcdd7cf0b48b
Tamara Acosta <tamara.ailen.acosta@gmail.com>
2020-10-21 19:55:48 -0300

    styling

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
@@ -6,34 +7,68 @@ import AccountCircle from '@material-ui/icons/AccountCircle';
 import InputBase from '@material-ui/core/InputBase';
 import SearchIcon from '@material-ui/icons/Search';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          width: '12ch',
+          '&:focus': {
+            width: '20ch',
+          },
+        },
+    },
+  }));
 
 
-class Menu extends Component {
-    render() {
-        return ( 
-            <div>
+const Menu = () => {
+
+    const classes = useStyles();
+
+    return (
+        <div>
                 <AppBar position="static">
                     <Toolbar>
-                        <MenuIcon></MenuIcon>
-                        <Typography variant="h6" >
-                        FitNess World
-                        </Typography>¨
+                        <IconButton className={classes.menuButton} color="inherit">
+                            <MenuIcon/>
+                        </IconButton>
+                        <Typography variant="h6" className={classes.title}>
+                        FITNESS
+                        </Typography>
                         <SearchIcon />
-                            <InputBase placeholder="Search product"/>
+                            <InputBase classes={{root: classes.inputRoot,input: classes.inputInput}} 
+                            placeholder="Search product"/>
                         <div className="menuI">
                             <Button color="inherit">Products</Button>
                             <Button color="inherit">contact us</Button>
                             <Button color="inherit">contact us</Button>
-                            <Button color="inherit">contact us</Button>
-                            <Button color="inherit">contact us</Button>
-                            <ShoppingCartIcon/>
-                            <AccountCircle/>
+                            <IconButton>
+                                <ShoppingCartIcon/>
+                            </IconButton>
+                            <IconButton>
+                                <AccountCircle/>  
+                            </IconButton>  
                         </div>
                     </Toolbar>
                 </AppBar>
             </div>
-        );
-    }
-}
+    );
+};
 
 export default Menu;
+
