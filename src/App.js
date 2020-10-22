43edb14c7315f18ad6c958ac2480d5a0aa54483a
Tamara Acosta <tamara.ailen.acosta@gmail.com>
2020-10-22 20:00:59 -0300

    add container

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 import Menu from './components/Menu';
+import  ListItems  from './components/ListItems';
+import { Container } from '@material-ui/core';
 
 function App() {
   return (
     <>
     <Menu></Menu>
+    <Container maxWidth="lg">
+        <ListItems/>
+    </Container>
     </>
   );
 }
